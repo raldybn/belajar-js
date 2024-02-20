92bda287605aba82254dc0cf14756336e0f635b3
raldybn <raldybelo@gmail.com>
2024-02-20 22:11:17 +0700

    first init github

---
+++ array.js
@@ -0,0 +1,14 @@
+let namaGuru = ["Kiki", "Agus", "Petrus"];
+namaGuru.push("dea", "vikri"); //untuk menambah nilai pada array tambahkan variable.push(nilai)
+namaGuru.shift(); //untuk menghapus nilai pada array bagian awal
+namaGuru.pop(); //untuk menghapus nilai pada array bagian akhir
+alert(`nama guru yang mengajar di kelas 4 adalah ${namaGuru}`);
+
+// atau bisa membuat array dengan cara
+
+let namaMurid = [];
+namaMurid[0] = "Adi";
+namaMurid[1] = "Angga";
+namaMurid[2] = "Ucok";
+namaMurid[3] = "Imang";
+alert(`nama murid yang belajar di kelas 4 adalah ${namaMurid}`);
---
+++ kuybelajar.js
@@ -0,0 +1,46 @@
+//variable=tipe data
+
+let nama = "Petrus Reynaldy"; //tipe string
+let usia = 25; // tipe integer
+let tinggiBadan = 175.5; // tipe float or double
+let beratBadan;
+let pacar = 2;
+
+beratBadan = 61;
+
+if (pacar == null) {
+  pacar = "belum punya";
+} else {
+  pacar = "udah punya";
+}
+// switch (pacar) {
+//   case 1:
+//     pacar = "punya 1 aja";
+//     break;
+//   case 2:
+//     pacar = "punya 2 pacar";
+//     break;
+//   default:
+//     pacar = "belum punya pacar";
+//     break;
+// }X
+alert(
+  `nama saya ${nama} saya berusia ${usia} tinggi badan saya ${tinggiBadan}Cm berat badan saya ${beratBadan}Kg dan saya ${pacar}`
+);
+
+// kedua
+let saldoAwal = 50000;
+let saldoTambahan = 80000;
+const hutang = 30000;
+const saldoAkhir = saldoAwal + saldoTambahan - hutang;
+
+alert(
+  `saldo awal saya RP.${saldoAwal} dan saldo tambahan saya adalah RP.${saldoTambahan} total saldo yang saya miliki adalah sebanyak RP.${saldoAkhir}`
+);
+
+//ketiga
+let x = 2;
+let y = 3;
+const z = 8;
+const a = x - y + z;
+alert(`hasil dari 2-3+8 adalah ${a}`);
