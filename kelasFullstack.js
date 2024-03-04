9305d0974adfca4fc3fce779631f99899a91fc71
raldybn <raldybelo@gmail.com>
2024-03-04 20:18:32 +0700

    belajar

--- kelasFullstack.js
+++ kelasFullstack.js
@@ -1,5 +1,15 @@
-let saldoAwal = 20000;
-let saldoTambahan = 1500;
+document.title = "raldy belo";
+const body = document.body;
+body.append("hello world");
 
-const saldoAkhir = saldoAwal + saldoTambahan;
-alert(`jumlah uang yang saya punya sekarang adalah sebesar ${saldoAkhir}`);
+const h1 = document.createElement("h1"); //<h1></h1>
+h1.textContent = "<marquee>ni h1</marquee>"; //<h1>ini h1</h1>
+const namaSaya = document.createElement("p");
+namaSaya.innerHTML = "<marquee>ini P<marque/>"; //text berjalan karna menggunakan tag marquee dan di dalam innerHTML
+
+const namaKamu = document.createElement("b");
+namaKamu.innerText = "<marquee>ini nama kamu<marquee/>";
+
+body.append(h1);
+body.append(namaSaya);
+body.append(namaKamu);
